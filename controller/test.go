82bd4245bc240f0854d0b6ba0640138ab8cabf80
马运宝 <mayunbao@yuanben.org>
2018-05-22 14:45:25 +0800

    修改结构

---
+++ controller/test.go
@@ -0,0 +1,10 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type TestController struct {
+
+}
+func (t TestController)Test(c *gin.Context)  {
+
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/SunMaybo/go-jewel/context"
+)
+
+func main() {
+	boot := context.NewInstance()
+   boot.RunWithConfigDirAndExtend("./config","www", func(cfgMap context.ConfigMap) {
+
+   })
+}
