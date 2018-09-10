3a9cc48740ebae6d1552138a0a4691b57b860076
运宝马 <yunbaoma@yunbaomadeMacBook-Pro.local>
2018-09-10 14:53:17 +0800

    添加性能监控

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"go-jewel/jewel"
 	"github.com/gin-gonic/gin"
 	"github.com/SunMaybo/jewel-inject/inject"
+	"go-jewel/jewel"
 )
 
 func main() {
-  jewel:=jewel.NewHttp()
+	jewel := jewel.NewHttp()
 	jewel.HttpStart(func(router *gin.RouterGroup, injector *inject.Injector) {
-		
+
 	})
 }
